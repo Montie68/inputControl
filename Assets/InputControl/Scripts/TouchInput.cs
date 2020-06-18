54f7d0b354d1f90df2e131537d54f936ee20ea42
geoff <geoff.newman@immersive.video>
2020-06-18 17:34:05 +1000

    EOD 18/06/2020
    Pinch in/out kinda working bug fixes need

--- Assets/InputControl/Scripts/TouchInput.cs
+++ Assets/InputControl/Scripts/TouchInput.cs
@@ -7,14 +7,17 @@ using UnityEngine.UIElements;
 public class TouchInput : InputControl
 {
 
-  #region Public
+    #region Public
     //public members go here
     public event EventHandler<directionMoveArgs> OnSwipe;
+    public event EventHandler<pinchMoveArgs> OnPinch;
     public event EventHandler OnPress;
     // minimum deley to register the touch as a press.
     public float touchPressTime = .5f;
     // the distance the fingers needs to move to clasifies as a swipe.
     public float minimumSwipeDistance = 1f;
+    // the distance the fingers needs to move to clasifies as a pinch.
+    public float minimumPinchDistance = 1f;
     // deley before restting the swipe direction
     public float swipeDeley = 0f;
 
@@ -23,14 +26,16 @@ public class TouchInput : InputControl
     [Header("* Remember to disable before building to device")]
     // [HideInInspector]
     public SWIPEDIRECTION swipeDirection = SWIPEDIRECTION.NONE;
-  #endregion
+    #endregion
 
-  #region Private
+    #region Private
     //private members go here
     private Touch theTouch;
+    private Touch[] theTouchs = new Touch[2];
     private float timeTouchEnded;
 
     private locationMoveData moveData = new locationMoveData();
+    private locationMoveData[] pinchData = { new locationMoveData(), new locationMoveData() };
 
     #endregion
     // Place all unity Message Methods here like OnCollision, Update, Start ect. 
@@ -44,13 +49,44 @@ public class TouchInput : InputControl
     public override void Update()
     {
 
-        // this is used convert mous pos to world pos
+        // this is used convert touch pos to world pos
         Vector3 VScreen = new Vector3();
         VScreen.x = Input.mousePosition.x;
         VScreen.y = Input.mousePosition.y;
         VScreen.z = Camera.main.transform.position.z;
 
-        if (Input.touchCount == 1 )
+        if (Input.touchCount == 2)
+        {
+            theTouchs[0] = Input.GetTouch(0);
+
+            theTouchs[1] = Input.GetTouch(1);
+
+            if (theTouchs[0].phase == TouchPhase.Began && theTouchs[1].phase == TouchPhase.Began)
+            {
+
+                // get the touch began and store the touch location
+                pinchData[0].firstTouch = theTouchs[0].position;
+
+                pinchData[1].firstTouch = theTouchs[1].position;
+
+            }
+            else if (theTouchs[0].phase == TouchPhase.Ended || theTouchs[1].phase == TouchPhase.Ended)
+            {
+                Vector2 pos = theTouchs[0].position;
+                pinchData[0].lastTouch = pos;
+                
+                pinchData[1].lastTouch = theTouchs[1].position;
+
+                // then invoke the Onswipe event.
+                  OnPinch?.Invoke(this, new pinchMoveArgs(pinchData));
+                // zero data
+                pinchData[0].zero();
+                pinchData[1].zero();
+                return;
+            }
+
+        }
+        else if(Input.touchCount == 1 )
         {
             // get the touch data if one finger is touching the screen
             theTouch = Input.GetTouch(0);
@@ -88,6 +124,7 @@ public class TouchInput : InputControl
 
 
         }
+         
         // mouse debuging
         if (mouseTesting) MouseDebug(VScreen);
         base.Update();
@@ -104,12 +141,15 @@ public class TouchInput : InputControl
         // subscribers to the touch events
         OnPress += Touch_OnPress;
         OnSwipe += Touch_OnSwipe;
+        OnPinch += Touch_Pinch;
     }
     void OnDisable()
     {
         // unsubscribers to the touch events
         OnPress -= Touch_OnPress;
         OnSwipe -= Touch_OnSwipe;
+        OnPinch -= Touch_Pinch;
+
     }
     //Place your public methods here
     public void Touch_OnPress(object sender, EventArgs e)
@@ -126,12 +166,21 @@ public class TouchInput : InputControl
         // the time deley before resetting the swipeDirection -1 doesn't reset the swipe
         if (swipeDeley > -1) StartCoroutine(SwipeRest());
     }
-
+    public void Touch_Pinch(object sender, pinchMoveArgs e)
+    {
+        Debug.Log("Touch Pinch! " + e.direction);
+    }
     public IEnumerator SwipeRest()
     {
         // wait before resetting the swipedirection 
         yield return new WaitForSeconds(swipeDeley);
         swipeDirection = SWIPEDIRECTION.NONE;
+    }
+    public IEnumerator SwipePinch()
+    {
+        // wait before resetting the swipedirection 
+        yield return new WaitForSeconds(swipeDeley);
+        
     }
     #endregion
     /**********************************************************/
@@ -191,6 +240,12 @@ public enum SWIPEDIRECTION
     LEFT,
     RIGHT,
 }
+public enum PINCHDIRECTION
+{
+    NONE = default,
+    IN,
+    OUT,
+}
 // class to store the first and last touch points then 
 [Serializable]
 public class locationMoveData
@@ -233,9 +288,9 @@ public class directionMoveArgs : EventArgs
         firstTouch = moveData.firstTouch;
         lastTouch = moveData.lastTouch;
         // create the vector of the first and last x coordinates
-        Vector2 xMag = new Vector2(firstTouch.x , lastTouch.x);
+        Vector2 xMag = new Vector2(firstTouch.x, lastTouch.x);
         // create the vector of the first and last y coordinates
-        Vector2 yMag = new Vector2(firstTouch.y , lastTouch.y);
+        Vector2 yMag = new Vector2(firstTouch.y, lastTouch.y);
 
         // horizontal test
         if (xMag.magnitude > yMag.magnitude)
@@ -266,6 +321,47 @@ public class directionMoveArgs : EventArgs
             }
         }
 
+    }
+
+
+}
+public class pinchMoveArgs : EventArgs
+{
+    // public getter of the direction
+    public PINCHDIRECTION direction
+    {
+        get
+        {
+            return _direction;
+        }
+    }
+    // vectors to hold the touch locations
+    Vector2[] firstTouch =  new Vector2[2];
+    Vector2[] lastTouch = new Vector2[2];
+    // direction of the swipe
+    private readonly PINCHDIRECTION _direction;
+
+    public pinchMoveArgs(locationMoveData[] moveData)
+    {
+        // assign the vectors from the passed movedate to the local vectors
+        firstTouch[0] = moveData[0].firstTouch;
+        lastTouch[0] = moveData[0].lastTouch;
+        firstTouch[1] = moveData[1].firstTouch;
+        lastTouch[1] = moveData[1].lastTouch;
+
+        float firstMag = Vector2.Distance(firstTouch[0], firstTouch[1]);
+        float lastMag = Vector2.Distance(lastTouch[0], lastTouch[1]);
+
+        Debug.Log("FirstMag: " + firstMag + "; Lastmag: " + lastMag);
+        // pinch distance test
+        if (firstMag > lastMag)
+        {
+            _direction = PINCHDIRECTION.IN;
+        }
+        else if (lastMag > firstMag)
+        {
+            _direction = PINCHDIRECTION.OUT;
+        }
     }
 
 
